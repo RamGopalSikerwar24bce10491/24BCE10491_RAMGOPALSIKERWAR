d85f8ec6bc36ccd583e29efc6c5e8b9c190566e2
Ram Gopal Sikerwar <ram.24bce10491@vitbhopal.ac.in>
2026-06-18 18:53:11 +0530

    Delete STUDENT RECORD SYSTEM/models/Department.js

--- STUDENT RECORD SYSTEM/models/Department.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const departmentSchema = new mongoose.Schema({
-    departmentName: { type: String, required: true },
-    departmentCode: { type: String, required: true, unique: true },
-    hod: { type: mongoose.Schema.Types.ObjectId, ref: 'Faculty' } // One-to-One
-}, { timestamps: true });
-
-module.exports = mongoose.model('Department', departmentSchema);
